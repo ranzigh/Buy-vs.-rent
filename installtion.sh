ceef77107da7b1f880022409bb6dca95a4f41542
ranzigh <72806562+ranzigh@users.noreply.github.com>
2021-02-27 13:07:23 +0100

    Create installtion.sh

---
+++ installtion.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+sudo update
+sudo upgrade
+
+sudo apt install python3 
+sudo apt install python-pip3 
+
+pip3 install notebook
+pip3 install numpy
+pip3 install pandas
+pip3 install matplotlib
