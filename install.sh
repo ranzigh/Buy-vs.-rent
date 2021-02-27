48323389fcfa279f81e6a0cec10d2ad8bb6a435e
ranzigh <72806562+ranzigh@users.noreply.github.com>
2021-02-27 13:19:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo upgrade
 sudo apt install python3 
 sudo apt install python-pip3 
 
-pip3 install notebook
+pip3 install notebook # start the notebook by typing 'jupyter notebook' into the terminal.
 pip3 install numpy
 pip3 install pandas
 pip3 install matplotlib
